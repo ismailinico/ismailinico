bacc5fd75d2dde25a69695f2f5ec9d1d8a74677d
Nico Ismaili <nico.ismaili@aoe.com>
2024-08-05 14:39:08 +0200

    fix: moved config to index.js
    /spend 7m

--- packages/eslint-config/eslint.config.js
+++ packages/eslint-config/eslint.config.js
@@ -1,30 +1,3 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { resolve } from "path";
+import configs from "./index.js";
 
-const projectTsConfig = resolve(process.cwd(), `.`, `tsconfig.json`);
-
-/** @type {import("eslint").Linter.Config} */
-export default [
-  js.configs.recommended,
-  {
-    name: `Nico Ismaili's ESLint Configuration`,
-    rules: {
-      quotes: [`error`, `backtick`],
-    },
-    languageOptions: {
-      globals: {
-        ...globals.jest,
-        ...globals.node,
-        ...globals.browser,
-      },
-    },
-    settings: {
-      "import/resolver": {
-        typescript: {
-          project: projectTsConfig,
-        },
-      },
-    },
-  },
-];
+export default configs;
--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,1 +1,30 @@
-export * from "./eslint.config";
+import js from "@eslint/js";
+import globals from "globals";
+import { resolve } from "path";
+
+const projectTsConfig = resolve(process.cwd(), `.`, `tsconfig.json`);
+
+/** @type {import("eslint").Linter.Config} */
+export default [
+  js.configs.recommended,
+  {
+    name: `Nico Ismaili's ESLint Configuration`,
+    rules: {
+      quotes: [`error`, `backtick`],
+    },
+    languageOptions: {
+      globals: {
+        ...globals.jest,
+        ...globals.node,
+        ...globals.browser,
+      },
+    },
+    settings: {
+      "import/resolver": {
+        typescript: {
+          project: projectTsConfig,
+        },
+      },
+    },
+  },
+];
